50baae5ea35acd21a278c89871426e678cd2a686
Teacher-Venkatesh <12venkat21@gmail.com>
2020-03-02 18:47:22 +0530

    added new progressions

--- src/controller/AstroFormController.java
+++ src/controller/AstroFormController.java
@@ -9,8 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import model.User;
-import service.AstroCalculator;
 
 @WebServlet(urlPatterns = {"/entervalue"})
 public class AstroFormController extends HttpServlet {
@@ -25,7 +23,7 @@ public class AstroFormController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+/* Uncomment the below code to run your application
 		String name=request.getParameter("name");
 		String gender=request.getParameter("gender");
 		String date=request.getParameter("dob1");
@@ -33,17 +31,13 @@ public class AstroFormController extends HttpServlet {
 		String year=request.getParameter("dob3");
 		String dob = date+"-"+month+"-"+year;
 	
+	// 1. ***Create an object for User in astro class and pass the inputs as contructor arguments.***
+	// 2. ***Create an object for User class and pass the values given above as arguments to constructor.***
+	// 3. ***Create an object for the AstroCalculator class.***
+	// 4. ***Call the findSign method and store the return value in a String variable called as astrosign***
 		
-		User astro = new User(name,gender,dob);
-		AstroCalculator zodiac=new AstroCalculator();
 		
-		astro.setName(name);
-		astro.setGender(gender);
-		astro.setDob(dob);
-		System.out.println(name);  
-		String astrosign=zodiac.findSign(astro);
-		System.out.println(astrosign);
-		
+	
 		
 		if(astrosign.equals("Aquarius")){
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
@@ -105,6 +99,6 @@ public class AstroFormController extends HttpServlet {
 			RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/Outputview.jsp");
 			request.setAttribute("astro", "<img src='./assets/virgo.svg'>");
 			rd.forward(request, response);
-		}
+		}*/
 	}
 }
--- src/model/User.java
+++ src/model/User.java
@@ -1,36 +1,3 @@
 package model;
 
-import java.time.LocalDate;
-
-public class User {
-	private String name;
-	private String gender;
-	private String dob;
-	public User(String name, String gender, String dob){
-		this.name = name;
-		this.gender = gender;
-		this.dob = dob;
-	}
-
-	public String getName() {
-		
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-		System.out.println(name);
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public String getDob() {
-		return dob;
-	}
-	public void setDob(String dob) {
-		this.dob = dob;
-}
-}
+//Type your code here
