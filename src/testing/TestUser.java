b5860ed7e2497fa183ad89f2df0cb9b880a6a394
FACEPrep-ProGrad <61002120+FACEPrep-ProGrad@users.noreply.github.com>
2020-03-26 11:21:43 +0530

    Create TestUser.java

---
+++ src/testing/TestUser.java
@@ -0,0 +1,30 @@
+package testing;
+
+/*Uncomment the below code to test
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import model.User;
+
+public class TestUser {
+	User astro = new User("Clara","female","03-11-1997");
+	@Test
+	public void testGetNameMethod() {
+		astro.setName("Clara");
+		astro.setGender("Female");
+		astro.setDob("03-11-1997");
+		assertEquals("Clara", astro.getName());
+		assertEquals("Female",astro.getGender());
+		assertEquals("03-11-1997",astro.getDob());
+		try {
+			astro.setName(null);
+			astro.setGender(null);
+			astro.setDob(null);
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
+*/
